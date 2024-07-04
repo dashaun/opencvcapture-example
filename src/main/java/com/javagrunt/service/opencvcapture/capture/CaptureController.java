4250586e96841b9667f25d02e3f5e92e25d359c1
dashaun <dashaun@dashaun.com>
2024-07-03 22:30:10 -0500

    building

--- src/main/java/com/javagrunt/service/opencvcapture/capture/CaptureController.java
+++ src/main/java/com/javagrunt/service/opencvcapture/capture/CaptureController.java
@@ -6,35 +6,51 @@ import org.bytedeco.javacv.OpenCVFrameConverter;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @RestController
 class CaptureController {
-    
-    @ResponseBody
-    @RequestMapping(value = "/capture", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
-    public byte[] live() throws IOException {
-        //This will grab an image from the default webcam (deviceNumber 0)
-        Mat grabbedImage;
+
+    private BufferedImage captureFrame() throws IOException {
         try (FrameGrabber grabber = new OpenCVFrameGrabber(0)) {
             grabber.start();
+            Mat grabbedImage;
             try (OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat()) {
                 grabbedImage = converter.convert(grabber.grab());
             }
             grabber.stop();
+            return Java2DFrameUtils.toBufferedImage(grabbedImage);
         }
-
-        BufferedImage image = Java2DFrameUtils.toBufferedImage(grabbedImage);
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        javax.imageio.ImageIO.write(image, "jpg", os);
-        return os.toByteArray();
     }
-    
+
+    // Endpoint to return captured image as JPEG byte array
+    @ResponseBody
+    @GetMapping(value = "/capture", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] getCapturedImage() {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            BufferedImage image = captureFrame();
+            javax.imageio.ImageIO.write(image, "jpg", os);
+            return os.toByteArray();
+        } catch (IOException e) {
+            return new byte[0];
+        }
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/encoded", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getEncodedImage() {
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            BufferedImage image = captureFrame();
+            javax.imageio.ImageIO.write(image, "jpg", os);
+            byte[] imageBytes = os.toByteArray();
+            return Base64.getEncoder().encodeToString(imageBytes);
+        } catch (IOException e) {
+            return "Error capturing image";
+        }
+    }
 }
--- src/test/java/com/javagrunt/service/opencvcapture/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.javagrunt.service.opencvcapture;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
